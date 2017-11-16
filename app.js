63d1d20b90eba129ed0dee4f8ea75a9e6b06f70a
GhorfiHassani <saleh.hassanighorfi@gmail.com>
2017-11-16 18:02:14 +0100

    First commit

---
+++ app.js
@@ -0,0 +1,54 @@
+var request = require('request');
+var restify = require('restify');
+var builder = require('botbuilder');
+
+
+// Partie Setup Creation d'une API 
+
+var server=restify.createServer();
+server.listen(process.env.port || process.env.PORT || 3978, function () {
+   console.log('%s listening to %s', server.name, server.url); 
+});
+
+// Create connector and bot
+var connector = new builder.ChatConnector({
+    appId: null,
+    appPassword: null
+});
+
+var bot = new builder.UniversalBot(connector);
+
+server.post('/api/messages', connector.listen());
+
+// Create the intent recognizer
+var dialog = new builder.IntentDialog();
+bot.dialog('/', dialog);
+
+
+//---------------------------------------------------------
+// Dialogs
+//---------------------------------------------------------
+
+dialog.matches(/^Qui etes avec moi ?/i, [
+        function (session) {
+            builder.Prompts.text(session, 'C est le robot de la sociéte CDiscount est vous  ?');
+        },
+        function (session, results) {        
+            /*openweathermap(results.response, function(success, previsions) {
+                if (!success) return session.send('Une erreur s\'est produite, veuillez réessayer.');
+                
+                var message = 'Voici la météo pour ' + results.response + ' :\n\n' +
+                              '_ Température : ' + previsions.temperature + '°C\n\n' + 
+                              '_ Humidité : ' + previsions.humidity + '%\n\n' +
+                              '_ Vent : ' + previsions.wind + 'km/h';
+                              
+                session.send(message);
+            });*/
+			var message ='Enchanté Monsieur '+results.response;
+			session.send(message);
+        }
+]);
+
+dialog.onDefault(function (session) {
+    session.send('Je n\'ai pas compris votre demande, il faut écrire "donne-moi la météo" !');
+});
